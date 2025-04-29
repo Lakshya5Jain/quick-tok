4aa27e4c2004d1b12d4381a71f9a09a479b9594b
ljain01 <ljain01@gradienttradingllc.com>
2025-04-28 21:19:33 -0400

    Edited Start-dev.sh

--- start-dev.sh
+++ start-dev.sh
@@ -13,9 +13,20 @@ supabase start
 
 # Deploy functions
 echo "Deploying Edge functions..."
+supabase functions deploy webhook-stripe
+supabase functions deploy create-checkout-session
+supabase functions deploy create-customer-portal-session
+supabase functions deploy get-user-credits
+supabase functions deploy add-monthly-credits
 supabase functions deploy generate-ai-video
-supabase functions deploy generate-script
+supabase functions deploy add-admin-credits
+supabase functions deploy cancel-subscription
 supabase functions deploy check-ai-video-status
+supabase functions deploy check-final-video-status
+supabase functions deploy create-final-video
+supabase functions deploy cleanup-files
+supabase functions deploy generate-script
+supabase functions deploy get-videos
 
 # Start frontend
 echo "Starting frontend application..."
